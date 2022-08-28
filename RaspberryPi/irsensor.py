0aa394fcdc346b6b7436a6c85bb0d800c3427676
yashvardhanraizada <yashvardhanraizada@gmail.com>
2022-08-28 20:03:16 +0530

    Infrared camera integration

--- RaspberryPi/irsensor.py
+++ RaspberryPi/irsensor.py
@@ -1,19 +1,24 @@
 import RPi.GPIO as GPIO
+import os
+
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 
 sensor_output = 16
-object_count_till_now = 0;
+image_count = 10
+capture_command_base = "libcamera-jpeg -o image"
+capture_command_parameters = "-t 5 --width 480 --height 480 --shutter 100000"
 
 GPIO.setup(sensor_output, GPIO.IN)
 
 while 1:
     if(GPIO.input(sensor_output) == 0):
         print("Object Detected")
-        object_count_till_now += 1
+        capture_command_final = capture_command_base + str(image_count) + ".jpg" + " " + capture_command_parameters;
+        os.system(capture_command_final)
+        image_count += 1
+        
         
     else:
         print("No Object Detected")
-        
-    print(object_count_till_now)
